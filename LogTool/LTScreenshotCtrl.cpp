45cb1622d723e20a5da31c7847e33cdf31e6795c
Sudantha Athauda <sudantha@millenniumit.com>
2013-09-04 16:00:49 +0530

    Screenshot Ctrl

--- LogTool/LTScreenshotCtrl.cpp
+++ LogTool/LTScreenshotCtrl.cpp
@@ -5,17 +5,30 @@
 #include "LTScreenshotCtrl.h"
 
 
+#define  SIDE_BUTTON_WIDTH   15
+
+#define  BTN_STATE_NORMAL	1
+#define  BTN_STATE_HOT	2
+#define  BTN_STATE_PRESSED	3
+#define  BTN_STATE_DISABLED	4
+
 // LTScreenshotCtrl
 
 IMPLEMENT_DYNAMIC(LTScreenshotCtrl, CWnd)
 
 LTScreenshotCtrl::LTScreenshotCtrl()
 {
-
+	p_PrevButton = NULL;
+	p_NextButton = NULL;
+	b_LeaveTracking = false;
 }
 
 LTScreenshotCtrl::~LTScreenshotCtrl()
 {
+	delete p_PrevButton;
+	delete p_NextButton;
+	CloseThemeData(h_thmEdit);
+	CloseThemeData(h_thmArrow);
 }
 
 
@@ -24,11 +37,22 @@ BEGIN_MESSAGE_MAP(LTScreenshotCtrl, CWnd)
 	ON_WM_SIZE()
 	ON_WM_CREATE()
 //	ON_WM_NCPAINT()
+//ON_WM_NCPAINT()
+ON_WM_MOUSEMOVE()
+ON_WM_LBUTTONDOWN()
+ON_WM_MOUSELEAVE()
+ON_WM_LBUTTONUP()
 END_MESSAGE_MAP()
 
 BOOL LTScreenshotCtrl::CreateScreenshotCtrl( CWnd* pParent, CRect rArea, int iID )
 {
-	return Create(NULL, NULL, WS_VISIBLE | WS_CHILD | WS_BORDER, rArea, pParent, iID);
+	//p_PrevButton = new ArrowButton(this, &h_thmSpin, SPNP_DOWNHORZ);
+	//p_NextButton = new ArrowButton(this, &h_thmSpin, SPNP_UPHORZ);
+	p_PrevButton = new ArrowButton(this, &h_thmArrow, SBP_ARROWBTN, 8);
+	p_NextButton = new ArrowButton(this, &h_thmArrow, SBP_ARROWBTN, 12);
+
+	Layout(rArea);
+	return Create(NULL, NULL, WS_VISIBLE | WS_CHILD /*| WS_BORDER*/, rArea, pParent, iID);
 }
 
 
@@ -46,13 +70,37 @@ void LTScreenshotCtrl::OnPaint()
 	CRect rClient;
 	GetClientRect(rClient);
 
-	dc.FillSolidRect(rClient, RGB(255,255,255));
+	DrawThemeParentBackground(m_hWnd, dc.m_hDC, rClient);
+	DrawThemeBackground(h_thmEdit, dc.m_hDC, EP_EDITBORDER_NOSCROLL, EPSN_NORMAL, r_Main, NULL);
+
+	p_NextButton->OnPaint(&dc);
+	p_PrevButton->OnPaint(&dc);
+}
+
+void LTScreenshotCtrl::Layout( CRect rClient )
+{
+	r_Main = rClient;
+	CRect rPrevButton = rClient;
+	CRect rNextButton = rClient;
+
+	r_Main.left += SIDE_BUTTON_WIDTH;
+	r_Main.right -= SIDE_BUTTON_WIDTH;
+	rPrevButton.right = r_Main.left - 1;
+	rNextButton.left = r_Main.right + 1;
+
+	rNextButton.bottom--;
+	rPrevButton.bottom--;
+
+	p_NextButton->SetRect(rNextButton);
+	p_PrevButton->SetRect(rPrevButton);
 }
 
 void LTScreenshotCtrl::OnSize(UINT nType, int cx, int cy)
 {
 	CWnd::OnSize(nType, cx, cy);
 
+	CRect rRect(0, 0, cx, cy);
+	Layout(rRect);
 	// TODO: Add your message handler code here
 }
 
@@ -61,13 +109,186 @@ int LTScreenshotCtrl::OnCreate(LPCREATESTRUCT lpCreateStruct)
 	if (CWnd::OnCreate(lpCreateStruct) == -1)
 		return -1;
 
+	h_thmEdit = OpenThemeData(m_hWnd, L"EDIT");
+	//h_thmSpin = OpenThemeData(m_hWnd, L"SPIN");
+	h_thmArrow = OpenThemeData(m_hWnd, L"SCROLLBAR");
+
+	p_NextButton->SetTheme(&h_thmArrow);
+	p_PrevButton->SetTheme(&h_thmArrow);
 	// TODO:  Add your specialized creation code here
 
 	return 0;
 }
+
+
 
 //void LTScreenshotCtrl::OnNcPaint()
 //{
 //	// TODO: Add your message handler code here
 //	// Do not call CWnd::OnNcPaint() for painting messages
 //}
+
+//void LTScreenshotCtrl::OnNcPaint()
+//{
+//	// TODO: Add your message handler code here
+//	// Do not call CWnd::OnNcPaint() for painting messages
+//
+//	Default();
+//	CWindowDC dc(this);
+//	CRect invRect;
+//	dc.GetClipBox (&invRect);
+//	//dc.FillSolidRect (&invRect,RGB (248,2,25));
+//
+//	if (IsThemeBackgroundPartiallyTransparent(h_thmEdit, EP_EDITBORDER_NOSCROLL, EPSN_NORMAL))
+//		DrawThemeParentBackground(m_hWnd, dc.m_hDC, invRect);
+//	DrawThemeBackground(h_thmEdit, dc.m_hDC, EP_EDITBORDER_NOSCROLL, EPSN_NORMAL, invRect, NULL);
+//}
+
+void LTScreenshotCtrl::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+
+	CClientDC dc(this);
+
+	p_NextButton->OnMouseMove(point, &dc);
+	p_PrevButton->OnMouseMove(point, &dc);
+
+	CWnd::OnMouseMove(nFlags, point);
+}
+
+void LTScreenshotCtrl::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+
+	CClientDC dc(this);
+	p_NextButton->OnMouseDown(point, &dc);
+	p_PrevButton->OnMouseDown(point, &dc);
+
+	CWnd::OnLButtonDown(nFlags, point);
+}
+
+void LTScreenshotCtrl::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+	CClientDC dc(this);
+	p_NextButton->OnMouseUp(point, &dc);
+	p_PrevButton->OnMouseUp(point, &dc);
+
+	CWnd::OnLButtonUp(nFlags, point);
+}
+
+
+void LTScreenshotCtrl::ArrowButton::OnMouseMove( CPoint point, CDC* pDC)
+{
+	int iOldState = i_State;
+
+	if (r_Area.PtInRect(point))
+	{
+		if (i_State == BTN_STATE_NORMAL)
+			i_State = BTN_STATE_HOT;
+		else if (i_State == BTN_STATE_PRESSED)
+			i_State = BTN_STATE_PRESSED; // No change
+	}
+	else
+	{
+		i_State = BTN_STATE_NORMAL;
+	}
+
+	if (i_State != iOldState)
+	{
+		DrawThemeBackground(*ph_Theme, pDC->m_hDC, i_Part, i_State + i_StateOffset, r_Area, NULL);
+		if (i_State != BTN_STATE_NORMAL)
+			p_Ctrl->TrackMouseLeave();
+	}
+}
+
+void LTScreenshotCtrl::ArrowButton::OnMouseDown( CPoint point, CDC* pDC)
+{
+	int iOldState = i_State;
+
+	if (r_Area.PtInRect(point))
+		i_State = BTN_STATE_PRESSED;
+	else
+		i_State = BTN_STATE_NORMAL;
+
+	if (i_State != iOldState)
+	{
+		DrawThemeBackground(*ph_Theme, pDC->m_hDC, i_Part, i_State + i_StateOffset, r_Area, NULL);
+		if (i_State != BTN_STATE_NORMAL)
+			p_Ctrl->TrackMouseLeave();
+	}
+}
+
+void LTScreenshotCtrl::ArrowButton::OnMouseUp( CPoint point, CDC* pDC)
+{
+	int iOldState = i_State;
+
+	if (r_Area.PtInRect(point))
+		i_State = BTN_STATE_HOT;
+	else
+		i_State = BTN_STATE_NORMAL;
+
+	if (i_State != iOldState)
+		DrawThemeBackground(*ph_Theme, pDC->m_hDC, i_Part, i_State + i_StateOffset, r_Area, NULL);
+}
+
+void LTScreenshotCtrl::ArrowButton::OnMouseLeave( CDC* pDC )
+{
+	int iOldState = i_State;
+	i_State = BTN_STATE_NORMAL;
+
+	if (i_State != iOldState)
+		DrawThemeBackground(*ph_Theme, pDC->m_hDC, i_Part, i_State + i_StateOffset, r_Area, NULL);
+}
+
+
+void LTScreenshotCtrl::ArrowButton::OnPaint( CDC* pDC )
+{
+	DrawThemeBackground(*ph_Theme, pDC->m_hDC, i_Part, i_State + i_StateOffset, r_Area, NULL);
+}
+
+
+LTScreenshotCtrl::ArrowButton::ArrowButton(LTScreenshotCtrl* pCtrl, HTHEME* pTheme, int iPart, int iStateOffset)
+{
+	i_State = BTN_STATE_NORMAL;
+	p_Ctrl = pCtrl;
+	ph_Theme = pTheme;
+	i_Part = iPart;
+	i_StateOffset = iStateOffset;
+}
+
+LTScreenshotCtrl::ArrowButton::~ArrowButton()
+{
+
+}
+
+
+
+void LTScreenshotCtrl::OnMouseLeave()
+{
+	// TODO: Add your message handler code here and/or call default
+	b_LeaveTracking = false;
+
+	CClientDC dc(this);
+	p_NextButton->OnMouseLeave(&dc);
+	p_PrevButton->OnMouseLeave(&dc);
+
+	CWnd::OnMouseLeave();
+}
+
+bool LTScreenshotCtrl::TrackMouseLeave()
+{
+	if (b_LeaveTracking)
+		return false;
+
+	TRACKMOUSEEVENT tTrkMouse;
+	tTrkMouse.cbSize = sizeof(TRACKMOUSEEVENT);
+	tTrkMouse.dwFlags = TME_LEAVE;
+	tTrkMouse.hwndTrack = m_hWnd;
+	TrackMouseEvent(&tTrkMouse);
+
+	b_LeaveTracking = true;
+
+	return false;
+}
+
--- LogTool/LTScreenshotCtrl.h
+++ LogTool/LTScreenshotCtrl.h
@@ -11,7 +11,52 @@ public:
 	LTScreenshotCtrl();
 	virtual ~LTScreenshotCtrl();
 
-	BOOL CreateScreenshotCtrl(CWnd* pParent, CRect rArea, int iID);
+	BOOL	CreateScreenshotCtrl(CWnd* pParent, CRect rArea, int iID);
+	void	Layout(CRect rClient);
+	bool	IsTrackingMouseLeave()	{	return b_LeaveTracking; };
+	bool	TrackMouseLeave();
+
+	class ArrowButton
+	{
+	public:
+		ArrowButton(LTScreenshotCtrl* pCtrl, HTHEME* pTheme, int iPart, int StateOffset = 0);
+		~ArrowButton();
+
+		void	SetTheme(HTHEME* pTheme) { ph_Theme = pTheme; };
+		void	SetRect(CRect rRect) { r_Area = rRect; };
+		void	OnMouseMove(CPoint point, CDC* pDC);
+		void	OnMouseDown(CPoint point, CDC* pDC);
+		void	OnMouseUp(CPoint point, CDC* pDC);
+		void	OnMouseLeave(CDC* pDC);
+		void	OnPaint(CDC* pDC);
+
+		LTScreenshotCtrl* p_Ctrl;
+		int		i_StateOffset;
+		int		i_State;
+		CRect	r_Area;
+		int		i_Part;
+		HTHEME* ph_Theme;
+	};
+
+	class Screenshot
+	{
+	public:
+
+	};
+
+	HTHEME h_thmEdit;
+	HTHEME h_thmArrow;
+
+	CRect r_Main;
+	bool  b_LeaveTracking;
+
+	ArrowButton* p_PrevButton;
+	ArrowButton* p_NextButton;
+
+	void	OnArrowButtonPress(ArrowButton* pButton);
+	void	OnArrowNext();
+	void	OnArrowPrevious();
+
 
 protected:
 	DECLARE_MESSAGE_MAP()
@@ -20,6 +65,11 @@ public:
 	afx_msg void OnSize(UINT nType, int cx, int cy);
 	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
 //	afx_msg void OnNcPaint();
+//	afx_msg void OnNcPaint();
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnMouseLeave();
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
 };
 
 
